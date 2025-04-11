2fdd666564db1505578d03cd7155465247006864
dgpenastein <dgpenastein@wpi.edu>
2025-04-11 14:05:49 +0200

    Add enviro demo code

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,8 @@ import os
 class ENV_VARS:
     MQTT_BROKER_HOST, MODULE_ID = None, None
     try:
-        MQTT_BROKER_HOST = os.getenv('MQTT_BROKER_HOST')
-        MODULE_ID = os.getenv("MODULE_ID")
+        MQTT_BROKER_HOST = os.getenv('MQTT_BROKER_HOST', "192.168.1.33")
+        MODULE_ID = os.getenv("MODULE_ID", "PI_MAC")
     except:
         print('Please set env vars!')
         exit(1)
